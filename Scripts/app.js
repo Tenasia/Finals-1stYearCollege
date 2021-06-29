50ece99760476002a25b75b669fec1a44ac768d2
Tenasia <alvicviojan@gmail.com>
2021-06-29 21:41:28 +0800

    commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,4 +4,23 @@ $(function() {
         $(this).addClass('active');
 
     });
+    $('.search-icon').click(function() {
+        $('.search-icon').addClass('display-none')
+        $('.search-icon1').removeClass('display-none')
+        $('.logo-icon').addClass('display-none')
+        $('.logo').addClass('display-none')
+        $('.search-bar').removeClass('display-none')
+        $('.cancel-button').removeClass('display-none')
+    });
+    $('.cancel-button').click(function() {
+        //
+        $('.cancel-button').addClass('display-none')
+        $('.search-icon').removeClass('display-none')
+        $('.search-icon1').addClass('display-none')
+        $('.search-bar').addClass('display-none')
+        $('.logo-icon').removeClass('display-none')
+        $('.logo').removeClass('display-none')
+
+
+    });
 });
